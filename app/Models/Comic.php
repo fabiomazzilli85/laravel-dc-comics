bea0543ffcd0803d5f245bda6a6ef5f4b450e771
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-29 16:31:08 +0200

    Inserito protected $fillable

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'author',
+        'publication_date',
+        'genre'
+    ];
 }
