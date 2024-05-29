f03eccb9a95b6b599263ce5c4db5ef58f9fdd703
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-29 17:28:10 +0200

    rimosso form per destroy (non ancora visto con gianluca), non funziona. rimosso form. rimosso csrf.

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,11 +6,5 @@
     <p><strong>Author:</strong> {{ $comic->author }}</p>
     <p><strong>Publication Date:</strong> {{ $comic->publication_date }}</p>
     <p><strong>Genre:</strong> {{ $comic->genre }}</p>
-    <a href="{{ route('comics.edit', $comic->id) }}">Edit</a>
-    <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" style="display:inline">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
     <a href="{{ route('comics.index') }}">Back to list</a>
 @endsection
