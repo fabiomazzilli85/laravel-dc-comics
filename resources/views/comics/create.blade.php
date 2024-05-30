b0384211610f13d19fa27c14a44c30bbd7eb92fc
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-30 14:22:50 +0200

    Ogni input in un div con class mb-3

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,24 +1,34 @@
 @extends('app')
 
 @section('content')
-    <h1 class="text-center my-5 text-danger">Add New Comic</h1>
-    <form action="{{ route('comics.store') }}" method="POST">
-        @csrf
+<h1 class="text-center my-5 text-danger">Add New Comic</h1>
+<form action="{{ route('comics.store') }}" method="POST">
+    @csrf
+    <div class="mb-3">
         <label for="title">Title:</label>
         <input type="text" name="title" id="title" required>
-        <br>
+    </div>
+
+    <div class="mb-3">
         <label for="description">Description:</label>
         <textarea name="description" id="description"></textarea>
-        <br>
+    </div>
+
+    <div class="mb-3">
         <label for="author">Author:</label>
         <input type="text" name="author" id="author" required>
-        <br>
+    </div>
+
+    <div class="mb-3">
         <label for="publication_date">Publication Date:</label>
         <input type="date" name="publication_date" id="publication_date" required>
-        <br>
+    </div>
+
+    <div class="mb-3">
         <label for="genre">Genre:</label>
         <input type="text" name="genre" id="genre" required>
-        <br>
-        <button type="submit" class="btn btn-outline-success mt-3">Aggiungi</button>
-    </form>
+    </div>
+
+    <button type="submit" class="btn btn-outline-success mt-3">Aggiungi</button>
+</form>
 @endsection
