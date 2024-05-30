b293a656fec37f9107a4e454370a645889f7eec2
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-30 14:15:45 +0200

    Lavorato su classi bootstrap.

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,27 +1,41 @@
 @extends('app')
 
 @section('content')
-    <h1 class="text-center my-5 text-danger">Edit Comic: {{ $comic->title }}</h1>
-    <form action="{{ route('comics.update', $comic) }}" method="POST">
-        
-        @csrf
-        @method('PUT')
-        
+<h1 class="text-center my-5 text-danger">Edit Comic: {{ $comic->title }}</h1>
+<form action="{{ route('comics.update', $comic) }}" method="POST">
+
+    @csrf
+    @method('PUT')
+
+    <div class="mb-3">
         <label for="title">Title:</label>
-        <input type="text" name="title" id="title" value="{{ $comic->title }}" required>
-        <br>
-        <label for="description">Description: </label>
-        <textarea name="description" id="description">{{ $comic->description }}</textarea>
-        <br>
+        <input class="form-control" type="text" name="title" id="title" value="{{ $comic->title }}" required>
+    </div>
+
+    <div class="mb-3">
+        <label for="description">Description:</label>
+        <textarea class="form-control" name="description" id="description">{{ $comic->description }}</textarea>
+    </div>
+
+    <div class="mb-3">
         <label for="author">Author:</label>
-        <input type="text" name="author" id="author" value="{{ $comic->author }}" required>
-        <br>
-        <label for="publication_date">Publication Date:</label>
-        <input type="date" name="publication_date" id="publication_date" value="{{ $comic->publication_date }}" required>
-        <br>
+        <input class="form-control" type="text" name="author" id="author" value="{{ $comic->author }}" required>
+    </div>
+
+    <div class="mb-3">
+        <div class="form-group mb-3">
+            <label for="publication_date">Publication Date:</label>
+            <input class="form-control" type="date" name="publication_date" id="publication_date"
+                value="{{ $comic->publication_date }}" required>
+        </div>
+    </div>
+
+    <div class="mb-3">
         <label for="genre">Genre:</label>
-        <input type="text" name="genre" id="genre" value="{{ $comic->genre }}" required>
-        <br>
-        <button type="submit">Update Comic</button>
-    </form>
+        <input class="form-control" type="text" name="genre" id="genre" value="{{ $comic->genre }}" required>
+    </div>
+  
+    <br>
+    <button class="btn btn-success my-3" type="submit">Update Comic</button>
+</form>
 @endsection
