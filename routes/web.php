722e9c066d6d8d875b936354a52801b2e6e7a576
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-30 13:47:01 +0200

    Implementato Route::resource('comics', ComicController::class) su consiglio di Gianluca.

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use App\Http\Controllers\ComicController;
 
 Route::resource('comics', ComicController::class);
+// Soluzione suggerita da Gianluca: con una sola riga di codice, prendo tutte le route.
