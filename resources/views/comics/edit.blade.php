b102bc492005808f80d8c9b2bbdbf80e2e917d8e
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-30 14:00:39 +0200

    Corretto refus minore

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,14 +1,16 @@
 @extends('app')
 
 @section('content')
-    <h1 class="text-center my-5 text-danger">Edit Comic</h1>
+    <h1 class="text-center my-5 text-danger">Edit Comic: {{ $comic->title }}</h1>
     <form action="{{ route('comics.update', $comic) }}" method="POST">
+        
         @csrf
         @method('PUT')
+        
         <label for="title">Title:</label>
         <input type="text" name="title" id="title" value="{{ $comic->title }}" required>
         <br>
-        <label for="description">Description:</label>
+        <label for="description">Description: </label>
         <textarea name="description" id="description">{{ $comic->description }}</textarea>
         <br>
         <label for="author">Author:</label>
