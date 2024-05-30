a5134459bd7448da7ab24e5738e491e6a41929f3
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-30 14:00:12 +0200

    Copiato div di Gianluca con  @nethod delete e button Delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,7 +7,16 @@
         @foreach ($comics as $comic)
             <li>
                 <a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a>
+                <div class="d-flex gap-2">
+                    <a href="{{ route('comics.edit', $comic->id) }}">Edit</a>
+                    <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn btn-link link-danger" type="submit">Delete Comic</button>
+                    </form>
+                </div>
             </li>
         @endforeach
     </ul>
 @endsection
+
